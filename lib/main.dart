e693c2866d26975742e120e0135f03b7b903d859
HadiHeikal <hadiiheikal@gmail.com>
2026-07-07 01:00:30 +0300

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:online_exam_app/features/auth/presentation/login/views/login_view.dart';
+
+void main() {
+  runApp(const OnlineExamApp());
+}
+
+class OnlineExamApp extends StatelessWidget {
+  const OnlineExamApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Online Exam App', home: const LoginView());
+  }
+}

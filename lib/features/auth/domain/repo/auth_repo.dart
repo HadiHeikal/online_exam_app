32be02e00921ec04a2c24c554775379a4f45bf43
HadiHeikal <hadiiheikal@gmail.com>
2026-07-15 15:56:49 +0300

    feat: add forgotPassword, verifyResetCode, and resetPassword methods to AuthRepo

--- lib/features/auth/domain/repo/auth_repo.dart
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -1,5 +1,6 @@
 import 'package:online_exam_app/config/base_response/base_response.dart';
 import 'package:online_exam_app/features/auth/domain/entities/auth_entity.dart';
+import 'package:online_exam_app/features/auth/domain/entities/message_entity.dart';
 
 abstract class AuthRepo {
   Future<BaseResponse<AuthEntity>> register({
@@ -15,5 +16,18 @@ abstract class AuthRepo {
   Future<BaseResponse<AuthEntity>> login({
     required String email,
     required String password,
+  });
+
+  Future<BaseResponse<MessageEntity>> forgotPassword({
+    required String email,
+  });
+
+  Future<BaseResponse<MessageEntity>> verifyResetCode({
+    required String resetCode,
+  });
+
+  Future<BaseResponse<MessageEntity>> resetPassword({
+    required String email,
+    required String newPassword,
   });
 }

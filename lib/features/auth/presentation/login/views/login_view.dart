a4f41d668220972275e819244698075b1ce193e3
Mohamed Elalfy <93038360+The1000y@users.noreply.github.com>
2026-07-18 19:12:24 +0300

    Add comment in LoginView widget
    Added a comment for clarification in the LoginView widget.

--- lib/features/auth/presentation/login/views/login_view.dart
+++ lib/features/auth/presentation/login/views/login_view.dart
@@ -14,6 +14,7 @@ class LoginView extends StatelessWidget {
       create: (context) => getIt.get<LoginCubit>(),
       child: LoginWidget(),
       //Try from alfy
+      //secound time
     );
   }
 }

cbb973017f8de0047350f810a4c80c9b1b920a75
HadiHeikal <hadiiheikal@gmail.com>
2026-08-02 19:49:00 +0300

    feat: add API endpoint constants for forgot password functionality

--- lib/core/constants/api_stings.dart
+++ lib/core/constants/api_stings.dart
@@ -1,3 +1,6 @@
 abstract class ApiStrings {
   static const String baseUrl = 'https://exam.elevateegy.com/api/v1/auth';
+  static const String forgotPasswordEndPoint = '/forgotPassword';
+  static const String verifyResetCodeEndPoint = '/verifyResetCode';
+  static const String resetPasswordEndPoint = '/resetPassword';
 }

27b17cbbaa1d592878e1a5a541f465aba146bb28
HadiHeikal <hadiiheikal@gmail.com>
2026-08-02 07:27:00 +0300

    feat: implement error handling in AuthRepoImpl for password recovery methods

---
+++ lib/features/auth/data/repo_impl/auth_repo_impl.dart
@@ -0,0 +1,55 @@
+import 'package:injectable/injectable.dart';
+import 'package:online_exam_app/config/base_response/base_response.dart';
+import 'package:online_exam_app/features/auth/data/data_source/remote/auth_remote_data_source.dart';
+import 'package:online_exam_app/features/auth/data/models/forget_password_models/message_response_model.dart';
+import 'package:online_exam_app/features/auth/domain/entities/message_entity.dart';
+import 'package:online_exam_app/features/auth/domain/repo/auth_repo.dart';
+
+@LazySingleton(as: AuthRepo)
+class AuthRepoImpl implements AuthRepo {
+  AuthRemoteDataSource authRemoteDataSource;
+  AuthRepoImpl(this.authRemoteDataSource);
+
+  @override
+  Future<BaseResponse<MessageEntity>> forgotPassword({
+    required String email,
+  }) async {
+    BaseResponse<MessageResponseModel> response = await authRemoteDataSource
+        .forgotPassword(email);
+    switch (response) {
+      case SuccessResponse<MessageResponseModel>():
+        return SuccessResponse<MessageEntity>(response.data.toEntity());
+      case ErrorResponse<MessageResponseModel>():
+        return ErrorResponse<MessageEntity>(response.error);
+    }
+  }
+
+  @override
+  Future<BaseResponse<MessageEntity>> verifyResetCode({
+    required String resetCode,
+  }) async {
+    BaseResponse<MessageResponseModel> response = await authRemoteDataSource
+        .verifyResetCode(resetCode);
+    switch (response) {
+      case SuccessResponse<MessageResponseModel>():
+        return SuccessResponse<MessageEntity>(response.data.toEntity());
+      case ErrorResponse<MessageResponseModel>():
+        return ErrorResponse<MessageEntity>(response.error);
+    }
+  }
+
+  @override
+  Future<BaseResponse<MessageEntity>> resetPassword({
+    required String email,
+    required String newPassword,
+  }) async {
+    BaseResponse<MessageResponseModel> response = await authRemoteDataSource
+        .resetPassword(email, newPassword);
+    switch (response) {
+      case SuccessResponse<MessageResponseModel>():
+        return SuccessResponse<MessageEntity>(response.data.toEntity());
+      case ErrorResponse<MessageResponseModel>():
+        return ErrorResponse<MessageEntity>(response.error);
+    }
+  }
+}

be6982a5ce6eb8c08f50dc487e71391efdab131c
HadiHeikal <hadiiheikal@gmail.com>
2026-08-04 13:30:55 +0300

    feat: add routing infrastructure with AppRouter and Routes classes

---
+++ lib/config/routing/routes.dart
@@ -0,0 +1,20 @@
+class Routes {
+  static const String loginView = '/loginView';
+  static const String registerView = '/registerView';
+  static const String forgotPasswordView = '/forgotPasswordView';
+  static const String verifyEmailView = '/verifyEmailView';
+  static const String resetPasswordView = '/resetPasswordView';
+
+  static const String homeView = '/';
+  static const String allExamsView = '/allExamsView';
+  static const String examInstructionsView = '/examInstructionsView';
+  static const String examView = '/examView';
+  static const String examScoreView = '/examScoreView';
+
+  static const String resultsView = '/resultsView';
+  static const String resultDetailsView = '/resultDetailsView';
+
+  static const String profileView = '/profileView';
+  static const String editProfileView = '/editProfileView';
+  static const String changePasswordView = '/changePasswordView';
+}

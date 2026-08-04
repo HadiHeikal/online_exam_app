6a48fb06d1945ad2468aa56ae8856d6706ba430c
HadiHeikal <hadiiheikal@gmail.com>
2026-08-04 13:33:12 +0300

    feat: implement error handling infrastructure with ErrorHandling and HandelErrorException classes

---
+++ lib/config/error/handel_error_exception.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:online_exam_app/config/error/error_handling.dart';
+
+class HandelErrorException {
+  String handelErrorexception(Exception error) {
+    if (error is DioException) return _handleDioException(error);
+    return 'something went wrong, pls try again';
+  }
+
+  String _handleDioException(DioException error) {
+    switch (error.type) {
+      case DioExceptionType.sendTimeout:
+        return 'send timeout';
+      case DioExceptionType.connectionTimeout:
+        return 'connection timeout';
+      case DioExceptionType.receiveTimeout:
+        return 'receive timeout';
+      case DioExceptionType.badCertificate:
+        return 'bad certificate';
+      case DioExceptionType.badResponse:
+        return ErrorHandling.handle(error).message!;
+      case DioExceptionType.cancel:
+        return 'request cancelled';
+      case DioExceptionType.connectionError:
+        return 'connection error';
+      case DioExceptionType.unknown:
+        return 'unknown';
+      case DioExceptionType.transformTimeout:
+        return 'transform timeout';
+    }
+  }
+}
